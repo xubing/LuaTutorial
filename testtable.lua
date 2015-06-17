814d1341c863b72798406ee7efd471c0170cd935
zhangxubing <zhxubing@gmail.com>
2015-06-17 22:01:05 +0800

    add table key demo

--- testtable.lua
+++ testtable.lua
@@ -6,6 +6,10 @@ local function printT(t)
     end
 end
 
+temp = 1
+tab = {[temp] = 1, 11}
+print('current :',tab[temp]) --此时的结果是11，因为11没有显式对应的key，因此从1开始，如果前面定义了，则覆盖其value
+
 
 mytable ={1,2,3,4,5,6}
 result = table.concat(mytable,'-',2,4)
